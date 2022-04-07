fa915e82b4787447736d19d9ebcfcdadc04a53e4
Armando Hernandez Castro <armandohernandezcastro102@gmail.com>
2022-04-07 09:59:13 -0500

    7/04/22

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,80 @@
+import {useState} from "react";
 
 function App() {
-  let varprueba= "TItulo";
-  const handleChangePrueba = (event) => { 
-    varprueba = event.target.value
+  //todo: presentar el conetopt de state
+  //hooks
+  const[inputState,setInputState] = useState({
+    titulo:"",
+    fecha:"",
+    nota:"",
+
+  });
+
+
+  const handleInputChange = (event) => { 
+    setInputState({
+      ...inputState,
+      [event.target.name]: event.target.value,
+    });
     
-  }
+  };
+
+  const handleLimpiarClick = () => {
+    setInputState(useState.titulo="asdasd");
+  };
   
+
   return (
     <div className="App">
+      <div className="row">
+      <div className="col">
+      <h3>Lista</h3>
+      </div>
+      <div className="col">
       <h3>notas</h3>
-      <label htmlFor="pruebaID">INput de prueba</label>
-      <input id="pruebaID" name="prueba" type="text" onChange={handleChangePrueba} value={varprueba} />
+      </div>
+      <label className="mb-2">Input de Titulos 
+      <input id="titulo"
+      name="titulo" 
+      type="text" 
+      onChange={handleInputChange} 
+      value={inputState.titulo} 
+      />
+      </label>
+          <br />
+          <br />
+
+      <label htmlFor="Fecha">INput de fecha </label>
+      <input id="fecha" 
+      name="fecha" 
+      type="text" 
+      onChange={handleInputChange} 
+      value={inputState.fecha} 
+      />
+      <br />
+      <br />
+
+      <label htmlFor="notaID">INput de nota </label>
+      <input id="nota" 
+      name="nota" 
+      type="text" 
+      onChange={handleInputChange} 
+      value={inputState.nota} 
+      />
+
+<div 
+      style={{display: "flex", 
+      justifyContent: "flex-end",
+      marginRight:"5px"}}>
+      <botton type="button" 
+      class="btn btn-secondary" 
+      onClick={handleLimpiarClick}>
+        Limpiar
+        </botton>
+
+        
+      </div>
+      </div>
     </div>
   );
 } 
